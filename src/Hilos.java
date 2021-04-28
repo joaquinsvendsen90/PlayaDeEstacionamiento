08f756ce59e902f60117313b3c1d979d05023508
ManuEsperanza <sojaku@gmail.com>
2021-04-28 18:23:20 -0300

    agrego bandera de acceso en el metodo run

--- src/Hilos.java
+++ src/Hilos.java
@@ -7,6 +7,7 @@ public class Hilos extends Thread {
 	//static String[] nombre = {"Hilo 0 - \"Ingreso de Vehículos por barrera 1\" T:19,1,4","Hilo 1 - \"Ingreso de Vehículos por barrera 2\" T:20,2,5","Hilo 2 - \"Ingreso de Vehículos por barrera 3\" T:21,3,6","Hilo 3 - \"Entrada al piso 1\" T:7","Hilo 4 - \"Salida del piso 1\" T:9","Hilo 5 - \"Subir al piso 2\" T:23,8","Hilo 6 - \"Bajar del piso 2\" T:10,24","Hilo 7 - \"Llegada a la barrera de salida 1\" T:11","Hilo 8 - \"Salida por barrera 1\" T:13,15","Hilo 9 - \"LLegada a la barrera de salida 2\" T:12","Hilo 10 - \"Salida por barrera 2\" T:14,16"};
 	
 	private int vinoDeSleep = 0;
+	boolean finalizado =  false;
 	public static boolean condicionHilo = true;
 	public static long tiempoInicio, tiempoFinal;
 	
@@ -38,7 +39,8 @@ public class Hilos extends Thread {
 				else{
 					monitor.dispararTransicion(this.transicionesHilo[i]);
 				}
-				if(condicionHilo == false) {
+				if(condicionHilo == false && finalizado ==  false) {
+					finalizado = true;
 					tiempoFinal  = System.currentTimeMillis();
 					System.out.println("Ejecución finalizada. Se tardó "+((tiempoFinal - tiempoInicio)) +" ms para que salgan " +Ventana.cantidadAutos +" autos del estacionamiento." );
 					break;
